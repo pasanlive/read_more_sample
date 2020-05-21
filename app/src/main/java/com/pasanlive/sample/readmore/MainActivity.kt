bae1e7e53d31f84653870ee9fd6dd8c250e1b8a1
Pasan Buddhika Weerathunga <pasan.buddhika@mybudget.com.au>
2020-05-21 23:20:09 +0530

    Initial code sample of read more

---
+++ app/src/main/java/com/pasanlive/sample/readmore/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.pasanlive.sample.readmore
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addReadMore(resources.getString(R.string.content), read_more_textview)
+        
+    }
+
+    private fun addReadMore(text: String, textView: TextView) {
+        val ss = SpannableString(text.substring(0, 270) + "... read more")
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                addReadLess(text, textView)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    ds.color = resources.getColor(R.color.colorPrimary, theme)
+                } else {
+                    ds.color = resources.getColor(R.color.colorPrimary)
+                }
+            }
+        }
+        ss.setSpan(clickableSpan, ss.length - 10, ss.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = ss
+        textView.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun addReadLess(text: String, textView: TextView) {
+        val ss = SpannableString("$text read less")
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                addReadMore(text, textView)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    ds.color = resources.getColor(R.color.colorPrimary, theme)
+                } else {
+                    ds.color = resources.getColor(R.color.colorPrimary)
+                }
+            }
+        }
+        ss.setSpan(clickableSpan, ss.length - 10, ss.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = ss
+        textView.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
